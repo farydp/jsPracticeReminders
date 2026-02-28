b98bacdb1000e2d20fdad911cf0d0259ccb04ee4
FarydP <Farydalejandro1997@hotmail.com>
2026-02-28 03:51:49 -0500

    I've reorganized some files and folders. I've eliminated mi-api folder, and i've moved all his child files into the back folder

--- backProyectPractice/index.js
+++ backProyectPractice/index.js
@@ -1,9 +1,28 @@
+require('dotenv').config();
+
+const dns = require("dns");
+
+dns.setDefaultResultOrder("ipv4first");
+
+const mongoose = require('mongoose');
+
 const express = require("express")
 
 const app = express()
 app.use(express.json()) // Ayuda a Express a leer Json. / Es un middleware
 
 const tasks = []
+
+console.log("URI que está usando la app:");
+console.log(process.env.MONGO_URI);
+
+await mongoose.connect(process.env.MONGO_URI, {
+  serverSelectionTimeoutMS: 10000,
+});
+
+// mongoose.connection.once('open', () => {
+//   console.log('✅ Conectado correctamente a MongoDB');
+// });
 
 
 app.get("/", (req, res) => {
